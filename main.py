2cee8f4ccdd0bac5503a548ca8a7799de1e1a9ea
cmorris2945 <cmorris2945@yahoo.com>
2023-05-27 23:35:57 -0400

    Created columns, image and my discription

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import streamlit as st
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+st.set_page_config(layout="wide")
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/lab_coat.jpg")
+
+with col2:
+    st.title("Chris Radston")
+    content = """
+        Hello! My name is Chris Radston. I am the Principal Bioinformatics Scientist for the State of Hawaii Center for 
+        Disease Control and Prevention. With a Masters degree in Computer Science and a PhD in Biomedicine and Computer Science, I bring a unique blend of expertise to the dynamic field of bioinformatics.
+        My role involves analyzing and interpreting complex biological data sets to help unravel the intricate mysteries of life. I apply advanced computational techniques and machine learning algorithms to 
+        make sense of genomic sequences, protein structures, and metabolic pathways. My research primarily focuses on public health-related topics such as identifying genetic risk factors for diseases, 
+        understanding the impact of environmental changes on pathogen evolution, and monitoring the spread of infectious diseases across the state.
+       """
+    st.info(content)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
